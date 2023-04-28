7f335d0c8fd91ba7438a0e738d21c7ba84f89c73
Lucas Luann <lucasluann10@gmail.com>
2023-04-27 22:53:08 -0400

    Controllers e cálculo imc

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  String _infoTexto = "Informe os dados!";
+
+  void _resetaCampos() {
+    pesoController.text = "";
+    alturaController.text = "";
+    _infoTexto = "Informe os dados!";
+  }
+
+  void _calcularImc() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text) / 100;
+      double imc = peso / (altura * altura);
+
+      if (imc < 18.6) {
+        _infoTexto = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoTexto = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoTexto = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoTexto = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoTexto = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40) {
+        _infoTexto = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Calculadora de IMC"),
         centerTitle: true,
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+        actions: [
+          IconButton(
+            onPressed: _resetaCampos,
+            icon: Icon(Icons.refresh),
+          ),
+        ],
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -50,7 +88,7 @@ class _HomeState extends State<Home> {
               child: Container(
                 height: 50.0,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: _calcularImc,
                   child: Text(
                     "Calcular",
                     style: TextStyle(
@@ -62,7 +100,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             Text(
-              "Info",
+              _infoTexto,
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.blue, fontSize: 25.0),
             ),
